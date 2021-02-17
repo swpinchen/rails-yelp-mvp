8b3c4767c0d4ccc4cf6db84c4447cb8a1ac039d6
Stuart Pinchen <s_pinchen@hotmail.com>
2021-02-17 18:42:08 +0900

    Completed Yelp MVP

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,8 +14,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(task_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
